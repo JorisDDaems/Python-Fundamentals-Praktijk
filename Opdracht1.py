7034f0d8182fb24589da67c20c5ed880a1d812bf
JorisDDaems <jorisddaems@gmail.com>
2020-12-11 16:17:06 +0100

    Assignment 1 cleared

--- Opdracht1.py
+++ Opdracht1.py
@@ -4,36 +4,38 @@ sick_days = randint(0, 11)
 
 calling_in_sick = False
 
+actually_sick = randint(0,1)
+kinda_sick = randint(0,1)
+dont_feel_like_it = randint(0, 1)
+
+     
+print(actually_sick, kinda_sick, dont_feel_like_it)
+print(sick_days)
 
 
 
 while (sick_days > 0):
 
+    if sick_days > 0:
+
+        if actually_sick == 1:
+            print(actually_sick)
+            calling_in_sick = True
+            sick_days -= 1
+            
+        elif (kinda_sick == 1 & dont_feel_like_it == 1):
+            print(kinda_sick, dont_feel_like_it)
+            calling_in_sick = True
+            sick_days -= 1
+        
+        else:
+            calling_in_sick = False
+            print(f'I\'m not that sick. I\'ll go to work')
+            break 
     
-    actually_sick = randint(0,1)
-    kinda_sick = randint(0,1)
-    dont_feel_like_it = randint(0, 1)
-
-    
-    print(actually_sick, kinda_sick, dont_feel_like_it)
-    print(sick_days)
-
-
-    if actually_sick == 1 & sick_days > 0:
-        calling_in_sick = True
-        sick_days -= 1
-
-    elif kinda_sick == 1 & dont_feel_like_it == 1 & sick_days > 0:
-        calling_in_sick = True
-        sick_days -= 1
-
-    else:
-        calling_in_sick = False
-        print(f'Will I call in sick? {calling_in_sick}. Now I still have {sick_days} left.')
-        continue
-
-
     print(f'Will I call in sick? {calling_in_sick}. Now I still have {sick_days} left.')
+
+
 
 
 
